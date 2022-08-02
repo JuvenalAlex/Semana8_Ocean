b671300324497608b74c04b61d8e9a55bef1f8e7
JuvenalAlex <61656798+JuvenalAlex@users.noreply.github.com>
2022-08-01 22:02:25 -0300

    Add files via upload

---
+++ react--router/src/App/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Manatee from '../components/Manatee/Manatee'
+import Narwhal from '../components/Narwhal/Narwhal'
+import Whale from '../components/Whale/Whale'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Marine Mammasls</h1>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/manatee">Manatee</Link>
+            </li>
+            <li>
+              <Link to="/narwhal">Narwhal</Link>
+            </li>
+            <li>
+              <Link to="/whale">Whale</Link>
+            </li>
+            <li>
+              <Link to="/whale/beluga">Beluga Whale</Link>
+            </li>
+            <li>
+              <Link to="/whale/blue">Blue Whale</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/manatee" element={<Manatee />}></Route>
+          <Route path="/narwhal" element={<Narwhal />}></Route>
+          <Route exact path="/whale" element={<Whale />}></Route>
+          <Route path="/whale/:type" element={<Whale />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
